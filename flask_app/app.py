0436a9278fb3aa334dc8fb8ee97cfab1bb01b956
John Morrison <jjosephmorrison@gmail.com>
2019-10-27 18:55:42 -0400

    :

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+from PIL import Image
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing import image
+from functions import *
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
+# Post method to run prediction model and return what each person/comp chose and who won
+@app.route('/predict', methods=['POST'])
+def predict():
+    return predict_winner()
 
 if __name__ == '__main__':
     app.run()
