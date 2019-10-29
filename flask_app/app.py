2fab80b64cce0f9478270424254c5116cc229e5b
Jordan Ireland <jordan.b.ireland@gmail.com>
2019-10-28 22:49:09 -0700

    Fixed error with loading model

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from PIL import Image
+from functions import predict_winner
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing import image
-from functions import *
 
 app = Flask(__name__)
 
@@ -12,15 +12,25 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+loaded_model = None
+
 @app.route('/play')
 @app.route('/play', methods=['POST'])
 # Post method to run prediction model and return what each person/comp chose and who won
 def play():
+    if loaded_model == None:
+        with open('model.json', 'r') as f:
+            model = model_from_json(f.read())
+
+        # Load in the model weights
+        model.load_weights('20_epochs.h5')
+
+
     if(request.method == 'POST'):
         user_image = image.load_img(request.files['file'], target_size = (150,150))
         user_image = image.img_to_array(user_image)
         user_image = np.expand_dims(user_image, axis = 0)
-        return render_template('play.html', predict=predict_winner(user_image))
+        return render_template('play.html', predict=predict_winner(user_image, model))
     else:
         return render_template('play.html', predict=None)
 
--- flask_app/functions.py
+++ flask_app/functions.py
@@ -1,20 +1,3 @@
-from tensorflow.keras.models import model_from_json
-from tensorflow.keras.preprocessing import image
-import os
-import numpy as np
-import random
-
-
-def image_input():
-    # Currently imports random file from test folder
-    folder = random.choice(os.listdir('../Rock-Paper-Scissors/test'))
-    filename = random.choice(os.listdir('../Rock-Paper-Scissors/test/' + folder))
-    img = image.load_img('../Rock-Paper-Scissors/test/' + folder + '/' + filename, target_size=(150, 150,))
-    img = image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    return img
-
-
 score = {'user':0, 'comp':0}
 past_choices = []
 past_computer = []
@@ -42,17 +25,7 @@ def rps(user_choice, computer_choice):
     
     return results
 
-
-# Load in the CNN model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-
-# Load in the model weights
-loaded_model.load_weights('20_epochs.h5')
-
-def predict_winner(user_image):
+def predict_winner(user_image, model):
     '''
     Take in image input of 300x300 pixels of rock,paper,scissors
     Use the model to predict if it was a rock,papper, or scissors
@@ -60,8 +33,10 @@ def predict_winner(user_image):
     Decide the winner
     '''
 
+    import random
+
     # Compile the model
-    loaded_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
     # Pull in the image and the model's prediction of it
     result = loaded_model.predict(user_image)
